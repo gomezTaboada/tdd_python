96748ec51d4be5b673b7590e6120446c154d2fda
BruceWillis1 <anthony_1101@hotmail.com>
2022-03-01 14:56:02 -0500

    url, placeholder view, and first-cut templates for my_lists

--- superlists/settings.py
+++ superlists/settings.py
@@ -46,6 +46,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "lists",
+    "accounts",
+]
+
+AUTH_USER_MODEL = "accounts.User"
+AUTHENTICATION_BACKENDS = [
+    "accounts.authentication.PasswordlessAuthenticationBackend",
 ]
 
 MIDDLEWARE = [
@@ -145,3 +151,9 @@ LOGGING = {
     },
     "root": {"level": "INFO"},
 }
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "noreply.test.user.123@gmail.com"
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
