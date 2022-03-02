35589f8c3b4679827f1573c9a68a8f9abdd953b4
BruceWillis1 <anthony_1101@hotmail.com>
2022-03-02 15:09:02 -0500

    prev integration with jenkins

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,9 +1,10 @@
+import os
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
-import os
+from .server_tools import reset_database
 
 MAX_WAIT = 10
 
@@ -28,19 +29,10 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.staging_server = os.environ.get("STAGING_SERVER")
         if self.staging_server:
             self.live_server_url = "http://" + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
-
-    def get_item_input_box(self):
-        return self.browser.find_element_by_id("id_text")
-
-    def add_list_item(self, item_text):
-        num_rows = len(self.browser.find_elements_by_css_selector("#id_list_table tr"))
-        self.get_item_input_box().send_keys(item_text)
-        self.get_item_input_box().send_keys(Keys.ENTER)
-        item_number = num_rows + 1
-        self.wait_for_row_in_list_table(f"{item_number}: {item_text}")
 
     @wait
     def wait_for(self, fn):
@@ -51,6 +43,16 @@ class FunctionalTest(StaticLiveServerTestCase):
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
         self.assertIn(row_text, [row.text for row in rows])
+
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id("id_text")
+
+    def add_list_item(self, item_text):
+        num_rows = len(self.browser.find_elements_by_css_selector("#id_list_table tr"))
+        self.get_item_input_box().send_keys(item_text)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        item_number = num_rows + 1
+        self.wait_for_row_in_list_table(f"{item_number}: {item_text}")
 
     @wait
     def wait_to_be_logged_in(self, email):
--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render, redirect
-from lists.models import List
-from lists.forms import ExistingListItemForm, ItemForm
+from django.shortcuts import redirect, render
 from django.contrib.auth import get_user_model
 
+from lists.forms import ExistingListItemForm, ItemForm, NewListForm
+from lists.models import List
 
 User = get_user_model()
 
 
 def home_page(request):
     return render(request, "home.html", {"form": ItemForm()})
+
+
+def new_list(request):
+    form = NewListForm(data=request.POST)
+    if form.is_valid():
+        list_ = form.save(owner=request.user)
+        return redirect(list_)
+    return render(request, "home.html", {"form": form})
 
 
 def view_list(request, list_id):
@@ -20,18 +28,6 @@ def view_list(request, list_id):
             form.save()
             return redirect(list_)
     return render(request, "list.html", {"list": list_, "form": form})
-
-
-def new_list(request):
-    form = ItemForm(data=request.POST)
-    if form.is_valid():
-        list_ = List()
-        list_.owner = request.user
-        list_.save()
-        form.save(for_list=list_)
-        return redirect(list_)
-    else:
-        return render(request, "home.html", {"form": form})
 
 
 def my_lists(request, email):
